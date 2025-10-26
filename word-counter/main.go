7b019afe929ff2ad127dec8388bf39222de68caa
VJ-2303 <vanaraj1018@gmail.com>
2025-10-26 18:01:22 +0530

    Added -b flag to count for number of bytes in addition to words and Lines and Write tests for it

--- word-counter/main.go
+++ word-counter/main.go
@@ -10,17 +10,21 @@ import (
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count Bytes")
 
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
 
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
+	}
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	wc := 0
--- word-counter/main_test.go
+++ word-counter/main_test.go
@@ -10,7 +10,7 @@ func TestCountWords(t *testing.T) {
 
 	exp := 4
 
-	res := count(b, false)
+	res := count(b, false, false)
 
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
@@ -22,9 +22,21 @@ func TestCountLines(t *testing.T) {
 
 	exp := 3
 
-	res := count(b, true)
+	res := count(b, true, false)
 
 	if exp != res {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("hello")
+
+	exp := 5
+
+	res := count(b, false, true)
+
+	if exp != res {
+		t.Errorf("Expected %d, got %d instead", exp, res)
 	}
 }

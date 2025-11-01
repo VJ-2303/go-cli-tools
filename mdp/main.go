488a6c8fb7754a8cab765859067ceeda68422b97
VJ-2303 <vanaraj1018@gmail.com>
2025-11-01 11:48:08 +0530

    Written tests for ParseContent and Run function using Golden file

--- mdp/main.go
+++ mdp/main.go
@@ -12,17 +12,18 @@ import (
 )
 
 const (
-	header = `<!DOCTYPE html>
-	<html>
-		<head>
-			<meta http-equiv="content-type" content="text/html; charset=utf-8">
-			<title>Markdown Preview Tool</title>
-		</head>
-	<body>
+	header = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Markdown Preview Tool</title>
+</head>
+<body>
 `
 	footer = `
-		</body>
-	</html>
+</body>
+</html>
 `
 )
 

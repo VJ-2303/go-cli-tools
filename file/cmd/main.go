c907b6c6b6d068953acd53e245d5e057b66eeff2
VJ-2303 <vanaraj1018@gmail.com>
2025-11-17 14:36:27 +0530

    Written basic CLI for writer for writing files with zeros

---
+++ file/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/VJ-2303/writer"
+
+func main() {
+	writer.Main()
+}
--- file/writer.go
+++ file/writer.go
@@ -1,6 +1,24 @@
 package writer
 
-import "os"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func Main() {
+	size := flag.Int("s", 0, "Size of the file in bytes")
+	file := flag.String("f", "", "File name to create")
+	flag.Parse()
+	if *file == "" {
+		fmt.Println("enter the file name")
+		os.Exit(1)
+	}
+	err := WriteToFile(*file, make([]byte, *size))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func WriteToFile(path string, data []byte) error {
 	err := os.WriteFile(path, data, 0o600)

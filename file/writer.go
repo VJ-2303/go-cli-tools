3cf6828bb5ab54f8ca2c0e6ced330966dc824fce
VJ-2303 <vanaraj1018@gmail.com>
2025-11-17 14:15:04 +0530

    Written More tests

--- file/writer.go
+++ file/writer.go
@@ -3,5 +3,9 @@ package writer
 import "os"
 
 func WriteToFile(path string, data []byte) error {
-	return os.WriteFile(path, data, 0o600)
+	err := os.WriteFile(path, data, 0o600)
+	if err != nil {
+		return err
+	}
+	return os.Chmod(path, 0o600)
 }
